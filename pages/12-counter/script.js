7ee128e931820335414546ae9bfa1105ae80ba29
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 21:06:21 -0500

    Add 12 solution

--- pages/12-counter/script.js
+++ pages/12-counter/script.js
@@ -1,2 +1,22 @@
-// TODO: Wire up the buttons to update the count.
-// TODO: Don't allow the count to go below 0.
+const buttons = document.querySelectorAll("button");
+const count = document.querySelector("#count");
+
+function handleButtonClick(event) {
+  // Get the textContent of 'count' - turn it into a number
+  const countValue = Number(count.textContent);
+
+  // Which button was clicked? "increment" or "decrement"?
+  if (event.target.id === "increment") {
+    count.textContent = countValue + 1;
+  } else if (event.target.id === "decrement") {
+    // Don't let the count go below 0
+    if (countValue > 0) {
+      count.textContent = countValue - 1;
+    }
+  }
+}
+
+// Attach the event listener to each button
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});

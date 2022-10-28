cc5af4847d90e22f9bbd22ea52899afc027f0987
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 21:34:14 -0500

    Add solution for 13

--- pages/13-contact/script.js
+++ pages/13-contact/script.js
@@ -1,3 +1,19 @@
-// TODO: Handle form submission (prevent default)
-// TODO: Capture the form data into an object and then output message in '#response' element
-// Example: "Thank you for your submission John Doe. We will be in touch shortly using your email: john@doe.com"
+const form = document.querySelector("form");
+const p = document.querySelector("p");
+
+function createResponseMessage(formInfo) {
+  return `
+    Thank you ${formInfo.name} for your message!
+    We will get back to you at ${formInfo.email}.
+  `;
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  const userInputs = Object.fromEntries(data.entries());
+  p.textContent = createResponseMessage(userInputs);
+}
+
+form.addEventListener("submit", handleSubmit);

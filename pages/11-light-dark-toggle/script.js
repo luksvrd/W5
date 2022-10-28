3fd52c194e8a2e7144fe985ab6266f9d305eac86
Manav Misra <manavm@visionwebsoft.com>
2022-10-27 20:36:41 -0500

    Add solution for 11

--- pages/11-light-dark-toggle/script.js
+++ pages/11-light-dark-toggle/script.js
@@ -1,4 +1,6 @@
-// TODO: Select the HTML element
+const html = document.querySelector("html");
+const input = document.querySelector("input");
 
-// TODO: Add 'dark' to its class list whenever the checkbox is checked
-// HINT: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+input.addEventListener("change", function () {
+  html.classList.toggle("dark");
+});

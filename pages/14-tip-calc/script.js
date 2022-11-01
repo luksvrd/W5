d33a5a4b2e8b34da1194481b92c169bd1cebe681
Manav Misra <manavm@visionwebsoft.com>
2022-10-31 20:13:37 -0500

    Add JS for tip calculator

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,8 +1,9 @@
-// TODO: Output the information showing voff, the tip amount and the total in 'p' tags
 const form = document.querySelector("form");
+const tipAmtP = document.querySelector("#tip-amt");
+const totalAmtP = document.querySelector("#total-amt");
 
 function calcTotalWithTip(total, tipPct) {
-  return total + total * (tipPct / 100);
+  return Number(total) + total * (tipPct / 100);
 }
 
 function handleSubmit(e) {
@@ -12,6 +13,15 @@ function handleSubmit(e) {
     total: e.target.total.value,
     tip: e.target.tip.value,
   };
+
+  const totalWithTip = calcTotalWithTip(submission.total, submission.tip);
+
+  renderTipInfo(totalWithTip, submission.tip);
+}
+
+function renderTipInfo(totalAmt, tipAmt) {
+  tipAmtP.textContent = `Tip Amount: $${tipAmt}`;
+  totalAmtP.textContent = `Total Amount: $${totalAmt}`;
 }
 
 form.addEventListener("submit", handleSubmit);

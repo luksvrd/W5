a79673b75ec021c4c07dbb98e8e5e0443265a040
Manav Misra <manavm@visionwebsoft.com>
2022-10-31 19:10:07 -0500

    Write a function to calculate the total amount

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,7 +1,9 @@
-// TODO:
-// TODO: Write a function to calculate the total amount
 // TODO: Output the information showing voff, the tip amount and the total in 'p' tags
 const form = document.querySelector("form");
+
+function calcTotalWithTip(total, tipPct) {
+  return total + total * (tipPct / 100);
+}
 
 function handleSubmit(e) {
   e.preventDefault();

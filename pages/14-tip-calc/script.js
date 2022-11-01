71ed13ec0c608c1fd3eea2a3354411dfc8a81ab3
Manav Misra <manavm@visionwebsoft.com>
2022-10-31 19:05:48 -0500

    Get the form submission as an object

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,3 +1,15 @@
-// TODO: Get the form submission as an object
+// TODO:
 // TODO: Write a function to calculate the total amount
 // TODO: Output the information showing voff, the tip amount and the total in 'p' tags
+const form = document.querySelector("form");
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const submission = {
+    total: e.target.total.value,
+    tip: e.target.tip.value,
+  };
+}
+
+form.addEventListener("submit", handleSubmit);

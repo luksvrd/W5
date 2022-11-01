924487a234678345ed8b5dc4a496b14096a3c506
Manav Misra <manavm@visionwebsoft.com>
2022-10-31 19:19:53 -0500

    Render the tip info to the page

--- pages/13-contact/script.js
+++ pages/13-contact/script.js
@@ -1,19 +1,22 @@
 const form = document.querySelector("form");
 const p = document.querySelector("p");
 
-function createResponseMessage(formInfo) {
+function createResponseMessage(contact) {
   return `
-    Thank you ${formInfo.name} for your message!
-    We will get back to you at ${formInfo.email}.
+    Thank you ${contact.name} for your message!
+    We will get back to you at ${contact.email}.
   `;
 }
 
 function handleSubmit(e) {
   e.preventDefault();
 
-  const data = new FormData(e.target);
-  const userInputs = Object.fromEntries(data.entries());
-  p.textContent = createResponseMessage(userInputs);
+  const formInfo = {
+    name: event.target.name.value,
+    email: event.target.email.value,
+  };
+
+  p.innerText = createResponseMessage(formInfo);
 }
 
 form.addEventListener("submit", handleSubmit);

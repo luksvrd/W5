1e56b9fef231958438f89d4cb33705e3368d6260
Manav Misra <manavm@visionwebsoft.com>
2022-11-01 20:54:16 -0500

    Add example for FormData usage

--- pages/5-form/script.js
+++ pages/5-form/script.js
@@ -1,2 +1,14 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-// TODO: Log all form values to the console
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // 'target' is like a caller-id for the event. Who caused this submit event?
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  // We need to add the checkboxes to the data object
+  data.toppings = formData.getAll("toppings");
+
+  console.log(data);
+});

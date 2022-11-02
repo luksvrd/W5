6b661884fc4a355cdf30d2626c8a0b6a0c4ffd9a
Manav Misra <manavm@visionwebsoft.com>
2022-11-01 19:40:27 -0500

    Add JS for dark mode button

--- pages/3-click/script.js
+++ pages/3-click/script.js
@@ -1,2 +1,8 @@
 const html = document.querySelector("html");
 // html.classList.toggle("dark");
+
+const darkModeButton = document.querySelector("button");
+
+darkModeButton.addEventListener("click", function () {
+  html.classList.toggle("dark");
+});

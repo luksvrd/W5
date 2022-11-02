cef838419609c765fa274265d9bc955cbf1b8ffd
Manav Misra <manavm@visionwebsoft.com>
2022-11-01 19:46:59 -0500

    Add partial solution for button clicks

--- pages/3-click/script.js
+++ pages/3-click/script.js
@@ -2,7 +2,17 @@ const html = document.querySelector("html");
 // html.classList.toggle("dark");
 
 const darkModeButton = document.querySelector("button");
+const main = document.querySelector("main");
 
 darkModeButton.addEventListener("click", function () {
   html.classList.toggle("dark");
+});
+
+main.addEventListener("click", function (event) {
+  if (
+    event.target.matches("button") &&
+    event.target.textContent === "Lottery"
+  ) {
+    console.log("You clicked the lottery button");
+  }
 });
